d2104ec8b4520c7b1cc31d0d95170786aefc2eb5
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-26 11:37:15 +0100

    setting up bottom tab navigation

--- goodbye_mymoney/App.tsx
+++ goodbye_mymoney/App.tsx
@@ -3,18 +3,81 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Add, Expenses, Reports, Settings } from "./screens";
+import { theme } from "./them";
+import { FontAwesome5, Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="dark" />
-      <Tab.Navigator>
-        <Tab.Screen name="Expenses" component={Expenses} />
-        <Tab.Screen name="Reports" component={Reports} />
-        <Tab.Screen name="Add" component={Add} />
-        <Tab.Screen name="Settings" component={Settings} />
+    <NavigationContainer theme={theme}>
+      <StatusBar style="light" />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: theme.colors.card,
+          },
+          headerTitleStyle:{
+            // textAlign:'center'
+          }
+        }}
+      >
+        <Tab.Screen
+          name="Expenses"
+          component={Expenses}
+          options={{
+            tabBarLabel: "Expenses",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5
+                name="money-bill"
+                size={30}
+                color={theme.colors.text}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Reports"
+          component={Reports}
+          options={{
+            tabBarLabel: "Reports",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons
+                name="analytics"
+                size={30}
+                color={theme.colors.text}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Add"
+          component={Add}
+          options={{
+            tabBarLabel: "Add",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons
+                name="add"
+                size={30}
+                color={theme.colors.text}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarLabel: "Settings",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons
+                name="settings"
+                size={30}
+                color={theme.colors.text}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
