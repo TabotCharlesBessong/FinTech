e67842e20cb86096456a870b2d946a19bb4ddf5f
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-26 13:09:59 +0100

    setting screen

--- goodbye_mymoney/screens/Settings.tsx
+++ goodbye_mymoney/screens/Settings.tsx
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { ListItem } from "../component";
+import { Entypo } from "@expo/vector-icons";
+import { theme } from "../them";
 
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
+    <View
+      style={{
+        flexDirection: "column",
+        width: "100%",
+        margin: 16,
+        borderTopRightRadius: 11,
+        borderTopLeftRadius: 11,
+        overflow: "hidden",
+        marginLeft:11,
+        paddingHorizontal:11
+      }}
+    >
+      <ListItem
+        label="Category"
+        detail={
+          <Entypo
+            name="chevron-thin-right"
+            color={theme.colors.text}
+            size={20}
+          />
+        }
+        onClick={() => {}}
+      />
+      <ListItem
+        label="Report a bug"
+        detail={
+          <Entypo
+            name="chevron-thin-right"
+            color={theme.colors.text}
+            size={20}
+          />
+        }
+        onClick={() => {}}
+      />
+      <ListItem
+        label="Erese all data"
+        onClick={() => {}}
+        isDestructive
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
