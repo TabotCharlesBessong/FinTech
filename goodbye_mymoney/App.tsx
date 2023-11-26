d3a6de7a0ec09bbc782d7af47cc3b323c05776ac
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-26 10:38:42 +0100

    setting up navigation

--- goodbye_mymoney/App.tsx
+++ goodbye_mymoney/App.tsx
@@ -1,24 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Add, Expenses, Reports, Settings } from "./screens";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text} >Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="dark" />
+      <Tab.Navigator>
+        <Tab.Screen name="Expenses" component={Expenses} />
+        <Tab.Screen name="Reports" component={Reports} />
+        <Tab.Screen name="Add" component={Add} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  text:{
-    fontSize:24,
-    textAlign:'center'
-  }
+  text: {
+    fontSize: 24,
+    textAlign: "center",
+  },
 });
---
+++ goodbye_mymoney/screens/index.ts
@@ -0,0 +1,5 @@
+
+export {default as Expenses} from './Expenses'
+export {default as Reports} from './Reports'
+export {default as Add} from './Add'
+export {default as Settings} from './Settings'
